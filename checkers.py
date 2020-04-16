4ebad1d50ae88cd01e0e855940337e41417b3e2c
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-16 07:01:00 +0000

    auto commit

--- checkers.py
+++ checkers.py
@@ -51,7 +51,7 @@ def make_move(moves,board,team):
             enemy_x = (my_move[1][0] - my_move[0][0])/2 + my_move[0][0]
             enemy_y = (my_move[1][1] - my_move[0][1])/2 + my_move[0][1]
             board[enemy_x][enemy_y] = 0
-        return(board,my_move)
+        return[board,my_move]
     else:
         return "loss"
     
