09aea1962f97958d617be7e3206519b9576062df
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-16 06:40:52 +0000

    auto commit

--- checkers.py
+++ checkers.py
@@ -1,5 +1,4 @@
 
-
 start_board = [
     [0,1,0,1,0,1,0,1],
     [1,0,1,0,1,0,1,0],
@@ -12,27 +11,32 @@ start_board = [
 ]
 
 remaining = [12,12]
-
+                
 def find_moves(board,team):
     reg_moves = []
     take_moves = []
     for i in range(8):
         for j in range(8):
             if board[i][j] == team:
-                if i+team in range(8) and j+1 in range(8):
-                    if board[i+team][j+1] == 0:
-                        reg_moves.append([[i,j],[i+team,j+1]])
-                    elif board[i+team][j+1] == -1*team:
-                        if i+2*team in range(8) and j+2 in range(8):
-                            if board[i+2*team][j+2] == 0:
-                                take_moves.append([[i,j],[i+2*team,j+2]])
-                if i+team in range(8) and j-1 in range(8):
-                    if board[i+team][j-1] == 0:
-                        reg_moves.append([[i,j],[i+team,j-1]])
-                    elif board[i+team][j-1] == -1*team:
-                        if i+2*team in range(8) and j-2 in range(8):
-                            if board[i+2*team][j-2] == 0:
-                                take_moves.append([[i,j],[i+2*team,j+2]])                    
+                for k in [-1,1]:
+                    if i+team in range(8) and j+k in range(8):
+                        if board[i+team][j+k] == 0:
+                            reg_moves.append([[i,j],[i+team,j+k]])
+                        elif board[i+team][j+k] == -1*team:
+                            if i+2*team in range(8) and j+2*k in range(8):
+                                if board[i+2*team][j+2*k] == 0:
+                                    take_moves.append([[i,j],[i+2*team,j+2*k]])
+            if board[i][j] == 2*team:
+                for k in [-1,1]:
+                    for l in [-1,1]:
+                        if i+l in range(8) and j+k in range(8):
+                            if board[i+l][j+k] == 0:
+                                reg_moves.append([[i,j],[i+l,j+k]])
+                            elif board[i+l][j+k] == -1*team:
+                                if i+2*team in range(8) and j+2*k in range(8):
+                                    if board[i+2*l][j+2*k] == 0:
+                                        take_moves.append([[i,j],[i+2*l,j+2*k]])
+                   
 	if len(take_moves) > 0:
         return take_moves
     else:
