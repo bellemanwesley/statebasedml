b1c30f06b3bb36396378e7ebd7a655da8f900fa9
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-16 22:01:59 +0000

    auto commit

--- checkers.py
+++ checkers.py
@@ -101,6 +101,7 @@ def play_game():
 def dec_dict_update(boards_moves,dec_dict):
     for x in boards_moves:
         board_key = matrix_int(x[0],2)
+        print board_key
         move_key = matrix_int(x[1],0)
         if board_key in dec_dict:
             if move_key in dec_dict[board_key]:
