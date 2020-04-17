7689378a1206e45e333990cca547d144f62843b8
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-17 01:08:25 +0000

    auto commit

--- checkers_smart.py
+++ checkers_smart.py
@@ -61,8 +61,8 @@ def make_move(moves,board,team,dec_dict):
                 if move_int in dec_moves:
                     move_hist = dec_moves[move_int]
                     move_weight = float(move_hist[0])/(move_hist[0]+move_hist[1])
-                    else:
-                        move_weight = 0.5
+                else:
+                    move_weight = 0.5
             move_factor = move_weight * random()
             moves_dec.append(move_factor)
             print moves_dec
