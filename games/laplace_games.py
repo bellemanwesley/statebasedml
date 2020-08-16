d23a02d4c2d534e87ab9878fae8695ce480e1f9b
Ubuntu <ubuntu@ip-172-31-24-184.us-east-2.compute.internal>
2020-08-16 17:14:03 +0000

    updated games

--- games/laplace_games.py
+++ games/laplace_games.py
@@ -7,7 +7,8 @@ import sys
 #STEP 1: Define initial state 
 def initial_state():
     initial = []
-    return initial
+    initial_team = 1
+    return initial, initial_team
 
 #STEP 2: Determine which moves you can make given state s
 def find_moves(s):
@@ -16,36 +17,52 @@ def find_moves(s):
     
 #STEP 3: Use Laplace's Law to determine which move to make
 def decide_move(moves,strengths):
-    pass
+    move = ""
+    return move
 
 #STEP 4: Make the move
-def make_move(s, moves):
-    #Transform s based on the chosen move
-    return s
+def make_move(s, move):
+    #Transform s and team based on the chosen move
+    return s, team
 
-#STEP 5: Determine the status of the game
+#STEP 5: Determine the status of the game. If the game is over, determine the winner.
 def game_status(s):
     #Return True if the game is not over
     #Return False if the game is over
     if CONDITION:
-        return True
+        return [True]
     else:
-        return False
+        return [False, winner]
 
-#STEP 6:
+#STEP 6: When the game is over 
 
+#Helper A: Create a transformation from a move in moves to a string
+def move_transform(move):
+    pass
 
-def play_game():
-    s = initial_state()
+#Helper B: Create a transformation from a state to a string
+def state_transform(s):
+    pass
+
+def play_game(all_strengths):
+    s, team = initial_state()
     con = True
+    game_choices = []
     while con:
         moves = find_moves(s)
-    
+        move = decide_move(moves, all_strengths[s])
+        game_choices.append([team,s,move])
+        s, team = make_move(s, move)
+        status = game_status(s)
+        con = status[0]
+    winner = status[1]
+    return []
     
 def main():
     iterations = 10 #Number of times you want to play the game
+    all_strengths = {}
     for i in range(iterations):
-        play_game()
+        play_game(all_strengths)
     
 
 main()
---
+++ games/tictactoe_state.py
@@ -0,0 +1,55 @@
+from random import random
+from random import randint
+import copy
+import json
+import sys
+
+#STEP 1: Define initial state 
+def initial_state():
+    initial = [[0,0,0],[0,0,0],[0,0,0]]
+    return initial
+
+#STEP 2: Determine which moves you can make given state s
+def find_moves(s):
+    moves = []
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if s[i][j] == 0:
+                moves.append([i,j])
+    return moves
+    
+#STEP 3: Use Laplace's Law to determine which move to make
+def decide_move(moves,strengths):
+    pass
+
+#STEP 4: Make the move
+def make_move(s, moves):
+    #Transform s based on the chosen move
+    return s
+
+#STEP 5: Determine the status of the game
+def game_status(s):
+    #Return True if the game is not over
+    #Return False if the game is over
+    if CONDITION:
+        return True
+    else:
+        return False
+
+#STEP 6:
+
+
+def play_game():
+    s = initial_state()
+    con = True
+    while con:
+        moves = find_moves(s)
+    
+    
+def main():
+    iterations = 10 #Number of times you want to play the game
+    for i in range(iterations):
+        play_game()
+    
+
+main()
