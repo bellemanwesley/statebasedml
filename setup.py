846078e580bc51b9b13f31b0d88e4d2b44c2e4be
Wesley Belleman <bellemanwesley@gmail.com>
2021-02-15 04:32:44 -0800

    does this work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='statebasedml',
     packages=find_packages(include=['statebasedml']),
-    version='0.0.1',
+    version='0.0.2',
     author='Wesley Belleman',
     author_email="bellemanwesley@gmail.com",
     description="Machine learning package for state based ML.",
