a43a7f4c85b2b203329ab2be6cde5d6dad20b227
Wesley Belleman <bellemanwesley@gmail.com>
2021-02-15 04:10:17 -0800

    see if this works...

---
+++ edit.sh
@@ -0,0 +1,8 @@
+python3 -m venv venv;
+source venv/bin/activate;
+pip install --upgrade pip;
+pip install wheel;
+pip install setuptools;
+pip install twine;
+pip install pytest==4.4.1;
+pip install pytest-runner==4.4;
